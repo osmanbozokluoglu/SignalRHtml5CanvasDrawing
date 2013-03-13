281ec24c2f30afd83c8acf858cbd3af9f441189b
Osman Bozokluoğlu <osmanbozokluoglu@hotmail.com>
2013-03-13 23:27:22 +0200

    First Commit

---
+++ src/SignalRHtml5/SignalRHtml5/Controllers/CizimController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SignalRHtml5.Controllers
+{
+    public class CizimController : Controller
+    {
+        //
+        // GET: /Cizim/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ src/SignalRHtml5/SignalRHtml5/Hubs/DrawHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRHtml5.Hubs
+{
+    public class DrawHub : Hub
+    {
+        public DrawHub()
+        {
+
+        }
+        public void DrawSomeThings(int x, int y, int z, string w)
+        {
+            Clients.Others.draw(x, y, z, w);
+        }
+
+        public void ClearCanvas()
+        {
+            Clients.All.clearCanvas();
+        }
+    }
+}
